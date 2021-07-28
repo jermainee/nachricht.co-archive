5ea945fa0655976702e9432c8fdaf3e62d7c2064
Jermaine Jonas Ernst <34961982+jermainee@users.noreply.github.com>
2021-07-28 21:08:56 +0200

    Change dir names to consistent date formats

--- Nachricht.co Backups/05. Juni 14.12/include/inc.content.php
+++
@@ -1,5 +0,0 @@
-<?
-$slogan = "Die selbstzerstörende Nachricht, welche du als Link verschicken kannst!";
-$placeholder = "Hey James! Ich wollte dich nur kurz fragen ob du ...";
-$text3 = "Kopiere diesen Link und sende ihn wem du möchtest. Die gespeicherte Nachricht wird sofort gelöscht, nachdem sie das erste mal geöffnet wurde.";
-?>
